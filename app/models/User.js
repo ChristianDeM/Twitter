48381433120473d3f3b4bcc36eddac2fbf01b098
ChristianDeM <Christian.d.martino@gmail.com>
2022-04-20 22:49:58 -0500

    Agregando pruebas de user

---
+++ app/models/User.js
@@ -0,0 +1,14 @@
+class User {
+    constructor(id,usernamer, name, bio, data, lastUpdate){
+        this.id=id,
+        this.usernamer=usernamer,
+        this.name=name,
+        this.bio=bio,
+        this.data=data,
+        this.lastUpdate=lastUpdate
+    }
+  
+
+}
+
+module.exports=User
--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,5 +1,9 @@
+const User = require('./../../app/models/User')
+
 describe ("prueba jest",() =>{
  test("prueba 1 ", ()=>{
-    expect(10).toBe(9)
+
+   const user = new User ( 1,"ChristainDm","chris","Biograqfia","data","que ondas")
+   expect(user.id).toBe(1)
  })
 })
